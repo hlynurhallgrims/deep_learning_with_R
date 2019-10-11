554ee1ca029d0e21ce8336f638e25f5107060768
hlynurhallgrims <hlynurh@gmail.com>
2019-10-11 00:04:28 +0000

    Dund. Birta myndir af nokkrum stöfum

--- ch2_1.R
+++ ch2_1.R
@@ -8,12 +8,20 @@ mnist <- dataset_mnist()
 prófun_myndir     <- mnist$test$x
 prófun_merkingar  <- mnist$test$y
 
+#Skoða mynd
+
+for (i in 1:15) {
+
+  tala <- þjálfun_myndir[i, , ]
+  plot(as.raster(tala, max = 255))
+
+}
 # Hanna net ---------------------------------------------------------------
 tauganet <- keras_model_sequential() %>% 
   layer_dense(units = 512, activation = "relu", input_shape = c(28 * 28)) %>% 
   layer_dense(units = 10, activation = "softmax")
 
-# Samsetja net ------------------------------------------------------------
+  # Samsetja net ------------------------------------------------------------
 tauganet %>% compile(
   optimizer = "rmsprop",
   loss = "categorical_crossentropy",
@@ -26,7 +34,7 @@ tauganet %>% compile(
 þjálfun_myndir <- þjálfun_myndir / 255
 prófun_myndir <- array_reshape(prófun_myndir, c(10000, 28 * 28))
 prófun_myndir <- prófun_myndir / 255
-#Merkingar
+#Merkingar  
 þjálfun_merkingar <- to_categorical(þjálfun_merkingar)
 prófun_merkingar <- to_categorical(prófun_merkingar)
 
@@ -36,3 +44,6 @@ tauganet %>% fit(þjálfun_myndir, þjálfun_merkingar, epochs = 5, batch_size =
 # Meta --------------------------------------------------------------------
 mæling <- tauganet %>% evaluate(prófun_myndir, prófun_merkingar)
 mæling
+
+tala <- þjálfun_myndir[5,,]
+þjálfun_myndir
